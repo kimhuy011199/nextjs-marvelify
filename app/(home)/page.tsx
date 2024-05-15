e1f4f09de93fd7278b2afe6f16e37cc7bb378e1e
kimhuy011199 <kimhuy011199@gmail.com>
2024-05-15 23:15:11 +0700

    Create home page

---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Footer from '@/components/footer';
+import Header from '@/components/header';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
