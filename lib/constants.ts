a9cb7a5c98609606863ec34ab8f8202cae0ab6d4
kimhuy011199 <kimhuy011199@gmail.com>
2024-05-19 12:28:10 +0700

    Create app routes constants

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  HOME: '/',
+  PRODUCT: '/products',
+  ACCOUNT_PROFILE: '/account',
+  ACCOUNT_ADDRESSES: '/account/addresses',
+  ACCOUNT_ORDERS: '/account/orders',
+  CART: '/cart',
+  CHECKOUT: '/checkout',
+};
