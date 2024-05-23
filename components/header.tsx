71866cf554a1e776974d63fca08531af62d35a29
kimhuy011199 <kimhuy011199@gmail.com>
2024-05-23 22:24:50 +0700

    Create header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Container from '@/components/container';
+import Logo from '@/components/logo';
+import AppMenu from '@/components/app-menu';
 
 const Header: React.FC = () => {
   return (
-    <div className="border-b py-6">
-      <Container>Header</Container>
+    <div className="border-b py-5 bg-background">
+      <Container>
+        <div className="flex justify-between items-center">
+          <Logo />
+          <AppMenu />
+        </div>
+      </Container>
     </div>
   );
 };
