74b8327753636f1f29c29e9c2594620ed3f0cba5
kimhuy011199 <kimhuy011199@gmail.com>
2024-05-25 13:33:49 +0700

    Create account settings page

--- app/(storefront)/account/page.tsx
+++ app/(storefront)/account/page.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import {
+  SettingCard,
+  SettingContent,
+  SettingDescription,
+  SettingHeading,
+} from '@/components/settings';
 
 const Page: React.FC = () => {
-  return <div>Profile</div>;
+  return (
+    <SettingCard>
+      <SettingHeading>Profile Settings</SettingHeading>
+      <SettingDescription>
+        View and update your profile information, including your name, email,
+        and phone number.
+      </SettingDescription>
+      <SettingContent>Account settings page</SettingContent>
+    </SettingCard>
+  );
 };
 
 export default Page;
