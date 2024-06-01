cb7019dc3c7915e587c47fd5a0062367dc0eb6bb
kimhuy011199 <kimhuy011199@gmail.com>
2024-06-01 15:06:42 +0700

    Init data for checkout process

--- components/checkout/checkout-container.tsx
+++ components/checkout/checkout-container.tsx
@@ -1,13 +1,23 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useCart } from '@/lib/hooks/use-cart';
 import NonSSRWrapper from '@/components/non-ssr-wrapper';
 import CheckoutForm from '@/components/checkout/checkout-form';
 import CheckoutSummary from '@/components/checkout/checkout-summary';
+import { useCheckout } from '@/lib/hooks/use-checkout';
 
 const CheckoutContainer: React.FC = () => {
   const cart = useCart();
+  const checkoutState = useCheckout();
+
+  useEffect(() => {
+    checkoutState.setCartItems(cart.items);
+    checkoutState.calculateSubTotal();
+    checkoutState.calculateTotal();
+    checkoutState.clearDiscount();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cart.items]);
 
   if (!cart.items.length) {
     return null;
