f90832323a23daf9a40bd1f04832639ba9d307ec
kimhuy011199 <kimhuy011199@gmail.com>
2024-06-01 15:06:00 +0700

    Get current checkout step

--- components/checkout/checkout-form.tsx
+++ components/checkout/checkout-form.tsx
@@ -6,22 +6,24 @@ import { CHECKOUT_STEPS } from '@/lib/constants';
 import CheckoutFormSummary from '@/components/checkout/checkout-form-summary';
 import CheckoutFormDelivery from '@/components/checkout/checkout-form-delivery';
 import CheckoutFormAddress from '@/components/checkout/checkout-form-address';
+import CheckoutFormPayment from '@/components/checkout/checkout-form-payment';
 import { useCheckout } from '@/lib/hooks/use-checkout';
+import { getCheckoutStep } from '@/lib/utils';
 
 const CheckoutForm: React.FC = () => {
-  const searchParams = useSearchParams();
-  const currentStep = searchParams.get('step') || CHECKOUT_STEPS.ADDRESS;
   const checkoutState = useCheckout();
-  console.log('checkoutState', checkoutState);
+  const searchParams = useSearchParams();
+  const paramStep = searchParams.get('step') || CHECKOUT_STEPS.ADDRESS;
+  const checkoutStep = getCheckoutStep(checkoutState.checkout, paramStep);
 
   const renderCheckoutStep = () => {
-    switch (currentStep) {
+    switch (checkoutStep) {
       case CHECKOUT_STEPS.ADDRESS:
         return <CheckoutFormAddress />;
       case CHECKOUT_STEPS.DELIVERY:
         return <CheckoutFormDelivery />;
       case CHECKOUT_STEPS.PAYMENT:
-        return null;
+        return <CheckoutFormPayment />;
       default:
         return null;
     }
@@ -29,7 +31,7 @@ const CheckoutForm: React.FC = () => {
 
   return (
     <div>
-      {/* <CheckoutFormSummary cart={cart} /> */}
+      <CheckoutFormSummary checkoutStep={checkoutStep} />
       {renderCheckoutStep()}
     </div>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { CHECKOUT_STEPS } from '@/lib/constants';
+import { CheckoutType } from '@/lib/types';
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
@@ -10,18 +11,21 @@ export const isValidCheckoutStep = (step: string) => {
   return Object.values(CHECKOUT_STEPS).includes(step);
 };
 
-export const getCheckoutStep = (cart: any, step: string = '') => {
+export const getCheckoutStep = (
+  checkoutState: CheckoutType,
+  step: string = ''
+) => {
   if (!isValidCheckoutStep(step) || step === CHECKOUT_STEPS.ADDRESS) {
     return CHECKOUT_STEPS.ADDRESS;
   } else if (step === CHECKOUT_STEPS.DELIVERY) {
-    if (!cart?.shippingAddress?.address) {
+    if (!checkoutState?.shippingAddress?.address1) {
       return CHECKOUT_STEPS.ADDRESS;
     }
     return CHECKOUT_STEPS.DELIVERY;
   } else if (step === CHECKOUT_STEPS.PAYMENT) {
-    if (!cart?.shippingAddress?.address) {
+    if (!checkoutState?.shippingAddress?.address1) {
       return CHECKOUT_STEPS.ADDRESS;
-    } else if (!cart?.shippingMethod?.value) {
+    } else if (!checkoutState?.deliveryMethod?.id) {
       return CHECKOUT_STEPS.DELIVERY;
     }
     return CHECKOUT_STEPS.PAYMENT;
