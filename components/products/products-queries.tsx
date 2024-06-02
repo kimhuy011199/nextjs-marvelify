93a01c65821bfbea3fbda94adb26be99cd53a9b9
kimhuy011199 <kimhuy011199@gmail.com>
2024-06-02 16:26:24 +0700

    Handle clear queries

--- components/products/products-queries.tsx
+++ components/products/products-queries.tsx
@@ -10,33 +10,56 @@ import {
   SEARCH_PARAMS_KEYS,
 } from '@/lib/constants';
 import { X } from 'lucide-react';
+import { usePathname, useRouter } from 'next/navigation';
+import { ProductSearchParamsInterface } from '@/lib/data/products';
 
-const ProductsQueries: React.FC = () => {
-  const handleClearFilters = () => {};
+interface ProductsQueriesProps {
+  searchParams: ProductSearchParamsInterface | undefined;
+}
+
+const ProductsQueries: React.FC<ProductsQueriesProps> = ({ searchParams }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const showClearFilters =
+    Object.keys(searchParams ?? {}).length > 1 ||
+    Object.values(searchParams ?? {}).some((value) => value !== '');
+
+  const handleClearFilters = () => {
+    const searchParams = new URLSearchParams('');
+    searchParams.append('', '');
+    router.replace(`${pathname}?${searchParams.toString()}`);
+  };
 
   return (
     <div>
       <p className="text-xl font-medium mb-4">Filtering Products:</p>
       <div className="flex flex-col gap-4">
-        <ProductsSort />
+        <ProductsSort key={searchParams?.sort || SEARCH_PARAMS_KEYS.SORT} />
         <ProductsFilter
+          key={
+            SEARCH_PARAMS_KEYS.AVAILABILITY + searchParams?.availability?.length
+          }
           filterKey={SEARCH_PARAMS_KEYS.AVAILABILITY}
           options={AVAILABILITY_OPTIONS}
           title="Availability"
         />
         <ProductsFilter
+          key={SEARCH_PARAMS_KEYS.FEATURE + searchParams?.feature?.length}
           filterKey={SEARCH_PARAMS_KEYS.FEATURE}
           options={FEAUTES_OPTIONS}
           title="Features"
         />
-        <Button
-          variant="outline"
-          onClick={handleClearFilters}
-          className="bg-white mt-2 self-start flex gap-1.5 px-3 hover:text-foreground"
-        >
-          <X size={14} />
-          <span>Clear</span>
-        </Button>
+        {showClearFilters ? (
+          <Button
+            variant="outline"
+            onClick={handleClearFilters}
+            className="bg-white mt-2 self-start flex gap-1.5 px-3 hover:text-foreground"
+          >
+            <X size={14} />
+            <span>Clear</span>
+          </Button>
+        ) : null}
       </div>
     </div>
   );
