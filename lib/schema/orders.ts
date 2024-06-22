8d76fe96df01cff752ec2af614ef5750dbf564a2
kimhuy011199 <kimhuy011199@gmail.com>
2024-06-22 15:47:54 +0700

    Create authentication schema

---
+++ lib/schema/orders.ts
@@ -0,0 +1,21 @@
+import { AddressType } from '@/lib/types';
+
+export type OrderInputDataType = {
+  email: string;
+  currency: string;
+  total: number;
+  subTotal: number;
+  paymentMethodId: string;
+  discount?: {
+    code: string;
+  };
+  shippingAddress: AddressType;
+  billingAddress?: AddressType;
+  lineItems: Array<{
+    productVariantId: string;
+    quantity: number;
+  }>;
+  deliveryMethod: {
+    id: string;
+  };
+};
