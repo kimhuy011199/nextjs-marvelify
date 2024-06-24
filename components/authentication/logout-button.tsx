cdff16e022bdb87aa185b9cee486996f4ce9a4a6
kimhuy011199 <kimhuy011199@gmail.com>
2024-06-25 03:51:05 +0700

    Handle logout

--- components/authentication/logout-button.tsx
+++ components/authentication/logout-button.tsx
@@ -1,17 +1,26 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { LogOut } from 'lucide-react';
+import { logout } from '@/lib/actions/authentication';
 
 const LogoutButton: React.FC = () => {
-  const handleLogout = () => {};
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogout = async () => {
+    setIsLoading(true);
+    await logout();
+    setIsLoading(false);
+  };
 
   return (
     <Button
       variant="outline"
       onClick={handleLogout}
       className="bg-white mt-2 self-start flex gap-1.5 px-3 hover:text-foreground w-28"
+      isLoading={isLoading}
+      disabled={isLoading}
     >
       <LogOut size={14} />
       <span>Log out</span>
