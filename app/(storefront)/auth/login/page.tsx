bd76d479f370eacebd24edfc3ea0ff94631f3acb
kimhuy011199 <kimhuy011199@gmail.com>
2024-06-25 03:50:42 +0700

    Create login page

--- app/(storefront)/auth/login/page.tsx
+++ app/(storefront)/auth/login/page.tsx
@@ -3,12 +3,20 @@ import Container from '@/components/container';
 import { Section } from '@/components/section';
 import { Metadata } from 'next';
 import LoginForm from '@/components/authentication/login-form';
+import { redirect } from 'next/navigation';
+import createServerClient from '@/lib/supabase/server';
 
 export const metadata: Metadata = {
   title: 'Login - Marvel Caseshop',
 };
 
-const Page: React.FC = () => {
+const Page: React.FC = async () => {
+  const supabase = createServerClient();
+  const { data } = await supabase.auth.getUser();
+  if (data?.user?.id) {
+    redirect('/');
+  }
+
   return (
     <Section className="py-16">
       <Container>
