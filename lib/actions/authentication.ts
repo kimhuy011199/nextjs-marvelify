5fb5d648c8d99817e4edbc27004aa71a55dc9f5d
kimhuy011199 <kimhuy011199@gmail.com>
2024-06-24 21:52:59 +0700

    Handle server authentication

---
+++ lib/actions/authentication.ts
@@ -0,0 +1,73 @@
+'use server';
+
+import { z } from 'zod';
+import { LoginSchema, RegisterSchema } from '@/lib/schema/authentication';
+import createServerClient from '@/lib/supabase/server';
+import { db } from '@/lib/db';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { ROUTES } from '@/lib/constants';
+
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  const validation = LoginSchema.safeParse(values);
+
+  if (!validation.success) {
+    throw new Error(validation.error.errors[0].message);
+  }
+
+  // Create supabase auth user
+  const supabase = createServerClient();
+  const supabaseAuthResponse = await supabase.auth.signInWithPassword({
+    email: values.email,
+    password: values.password,
+  });
+
+  if (supabaseAuthResponse.error) {
+    throw new Error(supabaseAuthResponse.error?.message);
+  }
+
+  revalidatePath('/', 'layout');
+  redirect('/');
+};
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validation = RegisterSchema.safeParse(values);
+
+  if (!validation.success) {
+    throw new Error(validation.error.errors[0].message);
+  }
+
+  // Create supabase auth user
+  const supabase = createServerClient();
+  const supabaseAuthResponse = await supabase.auth.signUp({
+    email: values.email,
+    password: values.password,
+  });
+
+  if (supabaseAuthResponse.error) {
+    throw new Error(supabaseAuthResponse.error?.message);
+  }
+
+  if (supabaseAuthResponse?.data?.user?.id) {
+    // Create user profile
+    await db.profile.create({
+      data: {
+        id: supabaseAuthResponse.data.user.id,
+        email: values.email,
+        name: values.name,
+      },
+    });
+
+    revalidatePath('/', 'layout');
+    redirect('/');
+  }
+
+  return 'Registered successfully';
+};
+
+export const logout = async () => {
+  const supabase = createServerClient();
+  await supabase.auth.signOut();
+  revalidatePath('/', 'layout');
+  redirect(ROUTES.LOGIN);
+};
