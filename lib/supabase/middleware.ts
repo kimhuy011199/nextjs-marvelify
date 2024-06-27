db5b932dcdac2d521ae603fe6c651de8c9f61065
kimhuy011199 <kimhuy011199@gmail.com>
2024-06-27 23:25:04 +0700

    Handle protected routes in middleware

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -1,5 +1,12 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
+import { ROUTES } from '@/lib/constants';
+
+const protectedRoutes = [
+  ROUTES.ACCOUNT_ADDRESSES,
+  ROUTES.ACCOUNT_PROFILE,
+  ROUTES.ACCOUNT_ORDERS,
+];
 
 const updateSession = async (request: NextRequest) => {
   let response = NextResponse.next({
@@ -54,7 +61,24 @@ const updateSession = async (request: NextRequest) => {
     }
   );
 
-  await supabase.auth.getUser();
+  // Handle protected routes
+  const currentUser = await supabase.auth.getUser();
+  if (
+    !currentUser?.data?.user &&
+    protectedRoutes.includes(request.nextUrl.pathname)
+  ) {
+    const absoluteURL = new URL(ROUTES.LOGIN, request.nextUrl.origin);
+    return NextResponse.redirect(absoluteURL.toString());
+  }
+
+  // Handle authentication on auth routes
+  if (
+    currentUser?.data?.user &&
+    request.nextUrl.pathname.includes(ROUTES.AUTH)
+  ) {
+    const absoluteURL = new URL(ROUTES.HOME, request.nextUrl.origin);
+    return NextResponse.redirect(absoluteURL.toString());
+  }
 
   return response;
 };
