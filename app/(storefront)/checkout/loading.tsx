cdfcc9a91ea543bf860613153d32fd8c52a96097
kimhuy011199 <kimhuy011199@gmail.com>
2024-06-30 22:40:26 +0700

    Add loading for checking out page

--- app/(storefront)/checkout/loading.tsx
+++ app/(storefront)/checkout/loading.tsx
@@ -5,7 +5,7 @@ const Loading: React.FC = () => {
   return (
     <div className="flex justify-center items-center w-full h-[80vh]">
       <div className="flex flex-col items-center justify-center gap-4">
-        <Spinner className="h-10 w-10 text-primary border-t-primary border-b-primary border-r-primary border-4" />
+        <Spinner />
         <span>Setting up checkout session...</span>
       </div>
     </div>
--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { twMerge } from 'tailwind-merge';
 
 interface SpinnerProps {
   className?: string;
@@ -8,10 +7,7 @@ interface SpinnerProps {
 const Spinner: React.FC<SpinnerProps> = ({ className }) => {
   return (
     <div
-      className={twMerge(
-        'inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white',
-        className
-      )}
+      className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
       role="status"
     >
       <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
