ec63707a92d8c48267a874b15916ca6f3d221fe4
kimhuy011199 <kimhuy011199@gmail.com>
2024-06-30 15:47:32 +0700

    Add app links to header

--- components/header.tsx
+++ components/header.tsx
@@ -3,6 +3,10 @@ import Container from '@/components/container';
 import Logo from '@/components/logo';
 import AppMenu from '@/components/app-menu';
 import CartTotalQuantityLink from '@/components/cart/cart-total-quantity-link';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { ROUTES } from '@/lib/constants';
+import { CircleUser, Search } from 'lucide-react';
 
 const Header: React.FC = () => {
   return (
@@ -11,6 +15,24 @@ const Header: React.FC = () => {
         <div className="flex justify-between items-center">
           <Logo />
           <div className="flex items-center gap-2">
+            <Button
+              asChild
+              variant="ghost"
+              className="p-2 rounded-md hover:text-primary hover:bg-transparent"
+            >
+              <Link href={ROUTES.SEARCH}>
+                <Search size={20} />
+              </Link>
+            </Button>
+            <Button
+              asChild
+              variant="ghost"
+              className="p-2 rounded-md hover:text-primary hover:bg-transparent"
+            >
+              <Link href={ROUTES.ACCOUNT_PROFILE}>
+                <CircleUser size={20} />
+              </Link>
+            </Button>
             <CartTotalQuantityLink />
             <AppMenu />
           </div>
